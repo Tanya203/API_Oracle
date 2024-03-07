48aa9befd500b0c1340b39463ba0ad91edc3762a
Tanya203 <opisme3@gmail.com>
2024-03-07 14:27:33 +0700

    thêm api contract type

---
+++ Controllers/ContractTypeApiController.cs
@@ -0,0 +1,25 @@
+﻿using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using test_api.Services;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ContractTypeApiController : Controller
+    {
+        private readonly ContractTypeServices _contractTypeServices;
+
+        public ContractTypeApiController(ContractTypeServices contractTypeServices)
+        {
+            _contractTypeServices = contractTypeServices;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllContractTypes()
+        {
+            var result = await _contractTypeServices.GetAllContractType();
+            return Ok(result);
+        }
+    }
+}
